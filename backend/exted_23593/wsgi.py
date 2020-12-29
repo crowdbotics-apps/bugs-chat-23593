93a4451fa0ef5bd3c2e520a3fac60e1debab0d1f
Crowdbotics <team@crowdbotics.com>
2020-12-29 14:16:24 +0000

    Update models

--- backend/exted_23593/wsgi.py
+++ backend/exted_23593/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exted_23593.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exted_23593.settings")
 
 application = get_wsgi_application()
